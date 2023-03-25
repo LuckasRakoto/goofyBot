dee407f78aabf8582d94ed846330c032c174a6f6
Luckas Rakoto <luckasrakoto@gmail.com>
2023-03-25 12:51:41 -0400

    init

---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	Token     string
+	BotPrefix string
+
+	config *configStruct
+)
+
+type configStruct struct {
+	Token     string `json:"token"`
+	BotPrefix string `json:"prefix"`
+}
+
+func ReadConfig() error {
+	file, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		panic(err)
+	}
+
+	err = json.Unmarshal(file, &config)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Printf(string(file))
+
+	err = json.Unmarshal(file, &config)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	Token = config.Token
+	BotPrefix = config.BotPrefix
+
+	return nil
+}
+
+var BotId string
+var goBot *discordgo.Session
+
+func Start() {
+	goBot, err := discordgo.New("Bot " + config.Token)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	u, err := goBot.User("@me")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	BotId = u.ID
+
+	goBot.AddHandler(messageHandler)
+
+	err = goBot.Open()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Printf("Bot is running")
+}
+
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == BotId {
+		return
+	}
+
+	if m.Content == BotPrefix+"ping" {
+		_, _ = s.ChannelMessageSend(m.ChannelID, "pong")
+		//s.ChannelMessageSend(m.ChannelID, "pong")
+	}
+}
+
+func main() {
+	err := ReadConfig()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	Start()
+
+	<-make(chan struct{})
+	return
+}
